b66935556a9bd76a16c3aadb44d479210406a4ad
Andre Ho <andrewhomin@gmail.com>
2025-09-18 17:08:30 -0300

    add Reserva class

--- src/Reserva.py
+++ src/Reserva.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
 class Reserva:
-    def __init__(self, ):
-        
+    def __init__(self, ano, mes, dia, hora, usuarioID, salaID):
+        self.data = datetime(ano, mes, dia, hora, 0, 0)
+        self.usuarioID = usuarioID
+        self.salaID = salaID
+    
